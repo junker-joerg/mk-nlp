a55f2c9af2a4c3562c849eb9da4606d9a6535e40
Köhler <MKoehler1@kpmg.com>
2019-03-12 11:44:23 +0100

    Dritter Versuch

---
+++ textblobTestMK01.py
@@ -0,0 +1,31 @@
+# Tests verschiedener NLP Tools
+# ! NLTK
+# ! Textblob
+# TODO commit
+# TODO  git push https://github.com/junker-joerg/mk-nlp
+# ! git remote add origin https://github.com/junker-joerg/mk-nlp
+# - eröffnet
+
+from textblob import TextBlobDE
+
+text = '''
+"Der Blob" macht in seiner unbekümmert-naiven Weise einfach nur Spass.
+Er hat eben den gewissen Charme, bei dem auch die eher hölzerne Regie und
+das konfuse Drehbuch nicht weiter stören.
+'''
+
+blob = TextBlobDE(text)
+blob.tags           # [('Der', 'DT'), ('Blob', 'NN'), ('macht', 'VB'),
+                    #  ('in', 'IN'), ('seiner', 'PRP$'), ...]
+
+blob.noun_phrases   # WordList(['Der Blob', 'seiner unbekümmert-naiven Weise',
+                    #           'den gewissen Charme', 'hölzerne Regie',
+                    #           'konfuse Drehbuch'])
+
+
+for sentence in blob.sentences:
+    print(sentence.sentiment.polarity)
+# 1.0
+# 0.0
+
+blob.translate(to="es")  # '" The Blob " hace a su manera ingenua...'

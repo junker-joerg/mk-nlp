b621f1d513221bd053d2e0cbf9c348c7988aeb7f
junker-joerg <mjkoehler1@gmx.de>
2019-03-13 21:46:49 +0100

    Abends

--- textblobTestMK02.py
+++ textblobTestMK02.py
@@ -1,33 +1,30 @@
 from __future__ import print_function, unicode_literals
 import spacy
-import de_core_news_sm
 
-nlp = spacy.load('de_core_news_sm')
+nlp = spacy.load('de')
 doc = nlp(u'Ich bin ein Berliner.')
 
 # show universal pos tags
-print(' '.join('{word}/{tag}'.format(word=t.orth_, tag=t.pos_) for t in doc))
+# print(' '.join('{word}/{tag}'.format(word=t.orth_, tag=t.pos_) for t in doc))
 # output: Ich/PRON bin/AUX ein/DET Berliner/NOUN ./PUNCT
 
 # show German specific pos tags (STTS)
-#print(' '.join('{word}/{tag}'.format(word.orth_, tag.tag_) for t in doc)) ! hier kommt noch ein Fehler 
+# print(' '.join('{word}/{tag}'.format(word.orth_, tag.tag_) for t in doc))
 # output: Ich/PPER bin/VAFIN ein/ART Berliner/NN ./$.
 
 # show dependency arcs
-print('\n'.join('{child:<8} <{label:-^7} {head}'.format(child=t.orth_, label=t.dep_, head=t.head.orth_) for t in doc))
+# print('\n'.join('{child:<8} <{label:-^7} {head}'.format(child=t.orth_, label=t.dep_, head=t.head.orth_) for t in doc))
 # output: (sb: subject, nk: noun kernel, pd: predicate)
 # Ich      <--sb--- bin
 # bin      <-ROOT-- bin
 # ein      <--nk--- Berliner
 # Berliner <--pd--- bin
 # .        <-punct- bin
-
 # show named entities
 for ent in doc.ents:
     print(ent.text)
 # output:
-# Berliner - nein!
-
+# Berliner
 # show noun chunks
 for chunk in doc.noun_chunks:
     print(chunk.text)
@@ -35,7 +32,7 @@ for chunk in doc.noun_chunks:
 # ein Berliner
 
 # noun chunks include so-called measure constructions ...
-doc = de(u'Ich möchte gern zum Essen eine Tasse Kaffee bestellen.') # TODO hängt wohl an den fehlenden Links
+doc = de(u'Ich möchte gern zum Essen eine Tasse Kaffee bestellen.')
 print([ chunk for chunk in doc.noun_chunks ])
 # output:
 # [Essen, eine Tasse Kaffee]
@@ -45,11 +42,10 @@ doc = de(u'Der Senator vermeidet das Thema Flughafen.')
 print([ chunk for chunk in doc.noun_chunks ])
 # output:
 # [Der Senator, das Thema Flughafen]
-
 # Use word vectors
 de = spacy.load('de')
 doc = de(u'Der Apfel und die Orange sind ähnlich')
-# assert len(doc.vector) == len(doc[0].vector))
+assert len(doc.vector) == len(doc[0].vector)
 der_apfel = doc[:2]
 die_orange = doc[3:5]
 der_apfel.similarity(die_orange)
